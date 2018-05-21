82790111de15a369bd8bf00ba75ceb1647ab2646
Bianca <bianca.rocha1711@gmail.com>
2018-05-21 07:46:51 -0300

    action show

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,9 @@
 module RecipesHelper
+  def calories_formater(recipe)
+    if recipe.light?
+      content_tag(:strong, "Light (#{recipe.calories} Calories)")
+    else 
+      content_tag(:strong, "#{recipe.calories} Calories")
+    end
+  end
 end
