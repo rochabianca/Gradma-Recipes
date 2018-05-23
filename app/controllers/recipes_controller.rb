9a3adfde32d4dd91c44768abb1c8d342bf419c7b
Bianca <bianca.rocha1711@gmail.com>
2018-05-23 08:49:35 -0300

    create and edit recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,4 +6,33 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
+
+  def edit 
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update 
+    @recipe = Recipe.find(params[:id])
+    params = recipe_params()
+    @recipe.update(params)
+
+    redirect_to @recipe
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    params = recipe_params()
+    @recipe = Recipe.new(params)
+    @recipe.save!
+    redirect_to @recipe
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :stuff, :calories, :prepare_mode, :cost)
+  end
 end
